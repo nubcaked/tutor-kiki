77a041b2f5151fa997a445c59ceb40174a005fe3
nubcaked <37624241+nubcaked@users.noreply.github.com>
2019-08-25 23:48:58 +0800

    added Navbar content

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import M from 'materialize-css';
 
 class Navbar extends Component {
+  componentDidMount() {
+    document.addEventListener('DOMContentLoaded', function() {
+      const sidenav = M.Sidenav.init(document.querySelectorAll('.sidenav'));
+      const collapsible = M.Collapsible.init(document.querySelectorAll('.collapsible'));
+    });
+    
+  }
+
   render() {
     return (
       <div>
-        
+        <nav className="nav-wrapper">
+          <div className="container">
+            <a href="#" className="brand-logo">Tutor.Kiki</a>
+            <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li><NavLink to="/">Class Schedule</NavLink></li>
+              <li><NavLink to="/">Fees & Promotions</NavLink></li>
+              <li><NavLink to="/">Parent Portal</NavLink></li>
+              <li><NavLink to="/">Tutor Portal</NavLink></li>
+              <li><NavLink to="/">Contact Us</NavLink></li>
+            </ul>
+          </div>
+        </nav>
+
+        <ul className="sidenav" id="mobile-demo">
+          <li><NavLink to="/">Class Schedule</NavLink></li>
+          <li><NavLink to="/">Fees & Promotions</NavLink></li>
+          <li><NavLink to="/">Parent Portal</NavLink></li>
+          <li><NavLink to="/">Tutor Portal</NavLink></li>
+          <li><NavLink to="/">Contact Us</NavLink></li>
+        </ul>
       </div>
     );
   }
