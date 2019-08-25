e1efa8e62f555bc3b005d1f569ab2c90df2c1cf8
nubcaked <37624241+nubcaked@users.noreply.github.com>
2019-08-25 13:29:58 +0800

    added dummy routes: Home, Login, Register | added layout Navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar.js';
+import Home from './components/route/Home';
+import Register from './components/route/Register';
+import Login from './components/route/Login';
+
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css';
 
 class App extends Component {
-
   render() {
     return (
-      <div>
-        
-      </div>
+      <BrowserRouter>
+        <Navbar />
+
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='register' component={Register} />
+          <Route path='login' component={Login} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
