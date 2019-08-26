ad1c5426dcfc819bff2086b34be6a1cb23756210
nubcaked <37624241+nubcaked@users.noreply.github.com>
2019-08-27 00:20:19 +0800

    installed node-sass | changed index.css to index.scss | added a Topbar.scss

--- src/components/layout/Topbar.js
+++ src/components/layout/Topbar.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import './Topbar.scss';
 
+//TODO: CREATE SCOPED STYLING OR USE STYLED-COMPONENTS
 class Topbar extends Component {
   render() {
     return (
       <div>
-
+        <nav className="top-bar">
+          <div className="container">
+            <ul id="nav-mobile" className="left">
+              <li><NavLink to="/">FAQ</NavLink></li>
+            </ul>
+            <ul id="nav-mobile" className="right">
+              <li><NavLink to="/">FAQ</NavLink></li>
+            </ul>
+          </div>
+        </nav>
       </div>
     );
   }
