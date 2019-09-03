edb9d5f807c64c06ee10cef9764703af28c3e598
nubcaked <37624241+nubcaked@users.noreply.github.com>
2019-09-03 22:31:05 +0800

    added contact information and FAQ to Topbar

--- src/components/layout/Topbar.js
+++ src/components/layout/Topbar.js
@@ -5,16 +5,26 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Hidden from '@material-ui/core/Hidden';
 import Container from '@material-ui/core/Container';
+import PhoneIcon from '@material-ui/icons/Phone';
+import MailIcon from '@material-ui/icons/Mail';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles(theme => ({
   button: {
     margin: theme.spacing(0),
   },
   container: {
-    backgroundColor: "pink",
+    backgroundColor: 'pink',
   },
   input: {
     display: 'none',
+  },
+  phoneIcon: {
+    verticalAlign: 'middle',
+  },
+  box: {
+    display: 'inline-block',
   },
 }));
 
@@ -27,13 +37,18 @@ export default function TextButtons(props) {
         <Container className={classes.container}>
           <Grid container justify="space-between" alignItems="center">
             <Grid item>
-              <Button component={Link} to="/test" className={classes.button}>
-                Class Schedule
-              </Button>
+              <Box className={classes.box} marginX="10px">
+                <PhoneIcon fontSize="small" className={classes.phoneIcon} />
+                <Typography variant="body2" display="inline">(+65)9169 9955</Typography>
+              </Box>
+              <Box className={classes.box}>
+                <MailIcon fontSize="small" className={classes.phoneIcon} />
+                <Typography variant="body2" display="inline">nubzcakez@gmail.com</Typography>
+              </Box>
             </Grid>
             <Grid item>
               <Button component={Link} to="/test" className={classes.button}>
-                Class Schedule
+                FAQ
               </Button>
             </Grid>
           </Grid>
